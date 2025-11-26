2918e2405936b377cbe6337983034370ae30361c
Dean Lynch <lynchdean@gmail.com>
2025-11-26 13:55:41 +0000

    drf tutorial final

---
+++ snippets/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from snippets import views
+
+# Create a router and register our ViewSets with it.
+router = DefaultRouter()
+router.register(r"snippets", views.SnippetViewSet, basename="snippet")
+router.register(r"users", views.UserViewSet, basename="user")
+
+# The router now determines the API URLs automatically.
+urlpatterns = [
+    path("", include(router.urls)),
+]
